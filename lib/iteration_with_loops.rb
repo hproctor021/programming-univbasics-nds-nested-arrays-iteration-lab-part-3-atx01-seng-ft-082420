b1b9199a5f242be92abd3caf5be5beb347d70a7b
Haley Proctor <hproctor021@gmail.com>
2020-08-08 16:06:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,13 +1,13 @@
 def join_nested_strings(src)
   count = 0
-  new_arrary = []
+  new_arrary = [""]
   while count < src.length do
     
     inner_count = 0
     while inner_count < src[count].length do
       if src[count][inner_count].class == String
        phrase = src[count][inner_count]
-      new_arrary << phrase.to_s
+      new_arrary << phrase
       end
       inner_count += 1
     end
